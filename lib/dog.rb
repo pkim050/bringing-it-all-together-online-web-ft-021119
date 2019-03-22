6d03d59a7ffaac72b320595d5c91807e126a949b
Patrick Kim <pkim050@gmail.com>
2019-03-22 09:02:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,7 +25,7 @@ class Dog
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     temp = DB[:conn].execute(sql, name).first
-    test = self.new(name: temp[1], breed: temp[2], id: [0])
+    temp2 = self.new(name: temp[1], breed: temp[2], id: [0])
     binding.pry
   end
 
